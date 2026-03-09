857aa6f7c49de25c5b1d46224855b82b01bf98b2
Alyona <sergeevaalyonasergeevna@gmail.com>
2026-03-09 21:43:16 +0800

    проверили, что кнопка работает

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -11,5 +11,10 @@ namespace WinFormsApp1
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Работает");
+        }
     }
 }
